c280af7acbbf05886b65158985c98cf73f275c98
Henrio <edenwannaji1980@gmail.com>
2026-01-30 11:18:28 +0100

    Switch to SQLite for immediate functionality

--- backend/intern_management/settings.py
+++ backend/intern_management/settings.py
@@ -68,12 +68,8 @@ WSGI_APPLICATION = 'intern_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='intern_management_db'),
-        'USER': config('DB_USER', default=''),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432', cast=int),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
